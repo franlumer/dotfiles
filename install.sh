b929947c1ed915eabe2b2f326a1950e861c66ffd
franlumer <franlumer09@gmail.com>
2025-12-06 21:25:36 +0000

    Agrego la instalacion de las dependencias de p10k y zsh

--- install.sh
+++ install.sh
@@ -17,3 +17,43 @@ ln -sf "$DOTFILES/.p10k.zsh" "$HOME/.p10k.zsh"
 unset file
 
 echo "Dotfiles instalados correctamente."
+
+#______________________________________________________________ REVISAR
+
+set -e
+
+echo "[+] Actualizando paquetes"
+sudo apt update -y && sudo apt upgrade -y
+
+echo "[+] Instalando Zsh"
+sudo apt install -y zsh git curl
+
+echo "[+] Cambiando shell por defecto a zsh"
+chsh -s "$(which zsh)"
+
+echo "[+] Instalando Oh My Zsh"
+export RUNZSH=no
+export CHSH=no
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo "[+] Instalando Powerlevel10k"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+      ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+echo "[+] Instalando autosuggestions y syntax-highlighting"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+echo "[+] Instalación completa"
+
